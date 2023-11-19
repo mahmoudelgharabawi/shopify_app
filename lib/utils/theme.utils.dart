cfc0a3400fda38e2d7e1e50fb259125ec9c076a2
mahmoudelgharabawi <m.elgharabawi@tecfy.co>
2023-11-19 22:16:40 +0200

    adding prefrences

---
+++ lib/utils/theme.utils.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ThemeUtils {
+  static ThemeData themeData = ThemeData(
+    textTheme: const TextTheme(
+        displayLarge: TextStyle(
+            color: Color(0xff617b33),
+            fontSize: 22,
+            fontWeight: FontWeight.w500),
+        displayMedium: TextStyle(
+            color: Colors.blue, fontSize: 18, fontWeight: FontWeight.w500)),
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+    useMaterial3: true,
+  );
+}
